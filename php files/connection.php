32fb7451b359079605df330d5a73a64624e2d250
vivianyeoh <khcy6yhw@nottingham.edu.my>
2017-03-30 14:58:14 +0800

    Delete connection.php

--- php files/connection.php
+++
@@ -1,7 +0,0 @@
-<?php	
-	
-	$hostname_localhost ="mysql.hostinger.my";
-	$database_localhost ="u299010794_nott";
-	$username_localhost ="u299010794_admin";
-	$password_localhost ="TZe8v7qNmlRD";
-?>
